03c3205f46c241e3a477b1b806af0e07cb368022
Chuks Emenike <chukschuksemenike@gmail.com>
2023-07-06 21:41:46 +0200

    created IAM role

--- jupiter-website-ecs/main.tf
+++ jupiter-website-ecs/main.tf
@@ -36,3 +36,9 @@ module "security_group" {
   source = "../modules/security-groups"
   vpc_id = module.vpc.vpc_id
 }
+
+# create ecs task execution
+module "create_ecs_task_execution" {
+  source          = "../modules/ecs-task-execution-role"
+  project_name    = module.vpc.project_name
+}
